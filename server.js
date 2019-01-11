e9ef0a1010a62f89b8178e145eb65574fcb1f15c
Bashanov Evgeny <e.bashanov@smstraffic.ru>
2019-01-11 13:50:41 +0300

    Added node express server

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.static(__dirname + '/dist'));
+
+app.listen(port, (err) => {
+  if (err) {
+    return console.log('Something bad happened', err)
+  }
+
+  console.log('Server is listening on ' + port)
+})
